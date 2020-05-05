e9f0fbba1738abace282efceae60ed947b50ae0a
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-05-05 16:25:27 +0200

    initial project commit

---
+++ ProductivityTools.Examples.PSMigration2Core.FrameworkSimple/TestExampleFrameworkSimple.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductivityTools.Examples.PSMigration2Core.FrameworkSimple
+{
+    [Cmdlet(VerbsDiagnostic.Test, "FrameworkSimple")]
+    public class TestExampleFrameworkSimple : System.Management.Automation.PSCmdlet
+    {
+        protected override void ProcessRecord()
+        {
+            Console.WriteLine("This is powershell module written in [.NET Framework 8.0]");
+            base.ProcessRecord();
+        }
+    }
+}
